1c09a959ca53de7463bd758a5a2730676a20a404
Nicole Aitchison <nlaitchison@gmail.com>
2014-04-16 19:09:35 -0400

    nav changes based on login/logout, moved login and logout functions to nav controller

--- BandmateAngular/app/scripts/controllers/NavCtrl.js
+++ BandmateAngular/app/scripts/controllers/NavCtrl.js
@@ -2,15 +2,35 @@
 
 /*global App*/
 
-App.controller('NavCtrl', function ($scope, AuthService) {
+App.controller('NavCtrl', function ($scope, Restangular, $http, AuthService, Base64, $location) {
 
 	console.log('nav', AuthService.isLoggedIn());
 	$scope.loggedIn = AuthService.isLoggedIn();
+
+	$scope.submit = function() {
+		console.log('meow');
+		var encoded = Base64.encode($scope.login.username + ':' + $scope.login.password);
+		console.log(encoded);
+
+		$http({method: 'POST', url: 'http://localhost:1337/auth/login', headers: {'Authorization': 'Basic ' + encoded}}).
+			success(function(data, status, headers, config){
+				console.log(data);
+				if (data.message === 'login successful'){
+					AuthService.setLoggedIn($scope.login.username, encoded);
+					console.log('nav ctrl:', AuthService.isLoggedIn());
+					$location.path('/profile/1');
+					$scope.loggedIn = AuthService.isLoggedIn();
+				}else{
+					alert('Invalid Username or Password!');
+				}
+			});
+	};
 
 	$scope.logout = function(){
 		AuthService.setloggedOut();
 		$scope.loggedIn = AuthService.isLoggedIn();
 		console.log('logout nav:', $scope.loggedIn);
+		$location.path('/');
 	};
 
 });
