e76a79f184868d41cfe46f3e27c9b7acb917ed8f
Nicole Aitchison <nlaitchison@gmail.com>
2014-04-17 17:57:36 -0400

    commented NavCtrl and UserCreateCtrl

--- BandmateAngular/app/scripts/controllers/NavCtrl.js
+++ BandmateAngular/app/scripts/controllers/NavCtrl.js
@@ -5,28 +5,52 @@
 App.controller('NavCtrl', function ($scope, Restangular, $http, AuthService, Base64, $location) {
 
 	console.log('nav', AuthService.isLoggedIn());
+
+	//set var for show and hide nav ul
 	$scope.loggedIn = AuthService.isLoggedIn();
 
+	// on login submit
 	$scope.submit = function() {
-		var encoded = Base64.encode($scope.login.username + ':' + $scope.login.password);
 
-		$http({method: 'POST', url: 'http://localhost:1337/auth/login', headers: {'Authorization': 'Basic ' + encoded}}).
-			success(function(data, status, headers, config){
-				if (data.message === 'login successful'){
-					AuthService.setLoggedIn($scope.login.username, encoded, data.user);
-					console.log('nav ctrl:', AuthService.isLoggedIn());
-					$location.path('/account/' + data.user.id);
-					$scope.loggedIn = AuthService.isLoggedIn();
-				}else{
-					alert('Invalid Username or Password!');
-				}
-			});
+		// encode for login
+        var encoded = Base64.encode($scope.login.username + ':' + $scope.login.password);
+
+        // login the user
+        $http({method: 'POST', url: 'http://localhost:1337/auth/login', headers: {'Authorization': 'Basic ' + encoded}}).
+          success(function(data, status, headers, config){
+
+            // if the user logged in
+            if (data.message === 'login successful'){
+
+              // set all the data for the user
+              AuthService.setLoggedIn($scope.login.username, encoded, data.user);
+
+              console.log('nav ctrl:', AuthService.isLoggedIn());
+
+              // take user to account page
+              $location.path('/account/' + data.user.id);
+
+              // set scope var for nav show and hide ul
+              $scope.loggedIn = AuthService.isLoggedIn();
+
+            }else{
+              alert('Invalid Username or Password!');
+            }
+          });
 	};
 
+	//on logout
 	$scope.logout = function(){
+
+		// call logout in authService
 		AuthService.setloggedOut();
+
+		//set var for show and hide nav ul
 		$scope.loggedIn = AuthService.isLoggedIn();
+
 		console.log('logout nav:', $scope.loggedIn);
+
+		// take user back to landing page
 		$location.path('/');
 	};
 
--- BandmateAngular/app/scripts/controllers/UserCreateCtrl.js
+++ BandmateAngular/app/scripts/controllers/UserCreateCtrl.js
@@ -4,26 +4,44 @@
 
 App.controller('UserCreateCtrl', function ($scope, Restangular, $http, AuthService, Base64, $location) {
   $scope.submit = function() {
+
+    // get users db
     var u = Restangular.all('users');
 
-    // $scope.signup.email = '';
-
+    // if the passwords in the signup match
     if($scope.signup.password === $scope.validate.password){
+
       console.log('match');
+
+      //set email to also equal the username field
       $scope.signup.email = $scope.signup.username;
-      console.log($scope.signup.email);
+
+      // add the user to the database then login the new user
       u.post($scope.signup).then(function(user){
+
         console.log(user);
 
+        // encode for login
         var encoded = Base64.encode($scope.signup.username + ':' + $scope.signup.password);
 
+        // login the user
         $http({method: 'POST', url: 'http://localhost:1337/auth/login', headers: {'Authorization': 'Basic ' + encoded}}).
           success(function(data, status, headers, config){
+
+            // if the user logged in
             if (data.message === 'login successful'){
+
+              // set all the data for the user
               AuthService.setLoggedIn($scope.signup.username, encoded, data.user);
+
               console.log('nav ctrl:', AuthService.isLoggedIn());
+
+              // take user to account page
               $location.path('/account/' + data.user.id);
+
+              // set scope var for nav show and hide ul
               $scope.loggedIn = AuthService.isLoggedIn();
+
             }else{
               alert('Invalid Username or Password!');
             }
@@ -31,9 +49,9 @@ App.controller('UserCreateCtrl', function ($scope, Restangular, $http, AuthServi
 
       });
     }else{
+
+      // if they don't match, do nothing yet.
       console.log('dont match');
     }
-
-    // console.log($scope.signup.email);
   };
 });
